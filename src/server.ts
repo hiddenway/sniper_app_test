dcd6dd531bd83b9a24aca143087ab561cb5accc3
hiddenway <thematerboox@gmail.com>
2025-11-10 18:44:01 +0700

    lint fixes

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import cors from "cors";
 import router from "./modules/trade/trade.controller";
 import config from "./config";
